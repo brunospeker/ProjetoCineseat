ca8971de07851bed4a49a84809f97f5fecae887e
brunospeker <bruno.speker@gmail.com>
2025-09-04 08:23:58 -0300

    Commit Inicial - Estrutura basica

---
+++ backend/Cineseat/src/main/java/com/maisprati/Cineseat/CineseatApplication.java
@@ -0,0 +1,13 @@
+package com.maisprati.Cineseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineseatApplication.class, args);
+	}
+
+}
---
+++ backend/Cineseat/src/test/java/com/maisprati/Cineseat/CineseatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maisprati.Cineseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
